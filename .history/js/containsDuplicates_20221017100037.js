dbfb785c2051f368cb5be9f9582082b618ea2a1f
Peter Tepela <kerenketepela@gmail.com>
2022-10-17 10:48:03 +0300

    Delete .history/js directory

--- .history/js/containsDuplicates_20221017100037.js
+++
@@ -1,8 +0,0 @@
-/**
- * Given an integer array nums, return true if any value appears at least 
- * twice in the array, and return false if every element is distinct.
- */
-const containsDuplicate = (arr) => {
-    let i = 0
-    for (i = 0; i.length++)
-}
